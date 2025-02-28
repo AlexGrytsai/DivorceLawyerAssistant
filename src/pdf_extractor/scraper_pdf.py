5309bf9e28c7ce8349ef2dff1702acc10af83d3f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-28 08:27:05 +0200

    feat: Implement basic PDF scraping functionality
    This commit introduces the base structure for scraping data from PDF files.
    It includes:
    
    - Adding `fitz` dependency.
    - A `BaseScraperPDF` abstract class with methods for opening the PDF, iterating through pages, and extracting text and widgets.
    - Placeholder methods for text and widget extraction, to be implemented in subclasses.

--- src/pdf_extractor/scraper_pdf.py
+++ src/pdf_extractor/scraper_pdf.py
@@ -1,7 +1,10 @@
 from abc import ABCMeta, abstractmethod
 
+import fitz
+
 
 class BaseScraperPDF(ABCMeta):
+    __slots__ = ("_file_path",)
     _file_path: str
 
     @abstractmethod
@@ -14,3 +17,15 @@ class ScraperPDF(BaseScraperPDF):
     @property
     def file_path(self) -> str:
         return self._file_path
+
+    def _extract_text_from_page(self, page: fitz.Page) -> None:
+        pass
+
+    def _extract_widgets_from_page(self, page: fitz.Page) -> None:
+        pass
+
+    def scrap_data(self) -> None:
+        doc: fitz.Document = fitz.open(self.file_path)
+        for page in doc:
+            self._extract_text_from_page(page)
+            self._extract_widgets_from_page(page)
