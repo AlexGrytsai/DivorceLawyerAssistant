57d99dd42ab549b5ce5eca12b08d933cc56bcb74
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-28 09:02:19 +0200

    feat: Implement text extraction from PDF pages
    This commit introduces the functionality to extract text with associated parameters (font, size, color, etc.) from individual pages of a PDF document.
    
    - Extracts text and its parameters using `fitz.Page.get_text("dict")`.
    - Filters out empty text spans to improve data quality.
    - Returns a list of dictionaries, each containing text and its parameters.
    - Adds pympler dependency to analyze object sizes

--- src/pdf_extractor/scraper_pdf.py
+++ src/pdf_extractor/scraper_pdf.py
@@ -1,14 +1,18 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod, ABC
+from typing import List, Dict, Any
 
 import fitz
+from pympler.asizeof import asizeof
 
 
-class BaseScraperPDF(ABCMeta):
+class BaseScraperPDF(ABC):
     __slots__ = ("_file_path",)
-    _file_path: str
 
-    @abstractmethod
+    def __init__(self, file_path: str) -> None:
+        self._file_path = file_path
+
     @property
+    @abstractmethod
     def file_path(self) -> str:
         return self._file_path
 
@@ -18,8 +22,22 @@ class ScraperPDF(BaseScraperPDF):
     def file_path(self) -> str:
         return self._file_path
 
-    def _extract_text_from_page(self, page: fitz.Page) -> None:
-        pass
+    @staticmethod
+    def __extract_text_from_page(page: fitz.Page) -> List[Dict[str, Any]]:
+        list_text_with_parameters: List[Dict[str, Any]] = []
+
+        for block in page.get_text("dict").get("blocks"):
+            block: dict[str, list]
+
+            if block.get("lines"):
+                for line in block["lines"]:
+                    line: dict
+
+                    for span in line.get("spans"):
+                        if span["text"].strip() != "":
+                            list_text_with_parameters.append(span)
+
+        return list_text_with_parameters
 
     def _extract_widgets_from_page(self, page: fitz.Page) -> None:
         pass
@@ -27,5 +45,5 @@ class ScraperPDF(BaseScraperPDF):
     def scrap_data(self) -> None:
         doc: fitz.Document = fitz.open(self.file_path)
         for page in doc:
-            self._extract_text_from_page(page)
+            text_with_parameters = self.__extract_text_from_page(page)
             self._extract_widgets_from_page(page)
