33cfdcfbe8513db0f19afa122e9aab1e23023a5d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-03 13:30:17 +0200

    feat: Implement table processing functionality
    This commit introduces the `TableProcessor` class, which inherits from `TableBaseProcessor` and provides concrete implementations for finding text lines within tables, updating pages to exclude these lines, and creating parser tables.
    
    The `find_text_lines_in_tables` method identifies lines that fall within the bounding boxes of tables on a given page. The `update_page_excluding_table_lines` method removes these identified lines from the page's list of lines, and `create_parser_table` currently does nothing, but is a placeholder for future table parsing logic.

--- src/pdf_extractor/table_processor.py
+++ src/pdf_extractor/table_processor.py
@@ -1,9 +1,59 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from itertools import chain
+from typing import List
+
+import pymupdf as fitz
+
+from src.pdf_extractor.geometry_utils import GeometryUtils
+from src.pdf_extractor.schemas import PagePDF, LinePDF
 
 
 class TableBaseProcessor(ABC):
-    pass
+    def __init__(self, geometry_utils: GeometryUtils) -> None:
+        self._geometry_utils = geometry_utils
+
+    @abstractmethod
+    def find_text_lines_in_tables(self, page: PagePDF) -> List[LinePDF]:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def update_page_excluding_table_lines(
+        table_lines: List[LinePDF], page: PagePDF
+    ) -> None:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def create_parser_table(table_lines: List[LinePDF], page: PagePDF) -> None:
+        pass
 
 
 class TableProcessor(TableBaseProcessor):
-    pass
+
+    def find_text_lines_in_tables(self, page: PagePDF) -> List[LinePDF]:
+        return list(
+            filter(
+                lambda line: any(
+                    self._geometry_utils.is_rect_inside(
+                        fitz.Rect(table.bbox), line.rect
+                    )
+                    for table in page.tables
+                ),
+                chain.from_iterable(page.lines for _ in page.tables),
+            )
+        )
+
+    @staticmethod
+    def update_page_excluding_table_lines(
+        table_lines: List[LinePDF],
+        page: PagePDF,
+    ) -> None:
+        table_lines_set = {id(line) for line in table_lines}
+        page.lines = [
+            line for line in page.lines if id(line) not in table_lines_set
+        ]
+
+    @staticmethod
+    def create_parser_table(table_lines: List[LinePDF], page: PagePDF) -> None:
+        pass
