54e7acdfc17fe2a2cf9564dfb87aead45b7f2c5d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-03 13:13:55 +0200

    feat: Implement `is_rect_inside` method for geometry utilities
    This commit introduces the `is_rect_inside` method to the `GeometryUtils` class. This method determines if a given rectangle (`inner_rect`) is completely contained within another rectangle (`outer_rect`), allowing for a specified tolerance.
    
    This functionality is crucial for accurately identifying and extracting text elements within specific regions of a PDF document.

--- src/pdf_extractor/geometry_utils.py
+++ src/pdf_extractor/geometry_utils.py
@@ -6,14 +6,44 @@ import pymupdf as fitz
 class GeometryBaseUtils(ABC):
     @staticmethod
     @abstractmethod
-    def is_same_line(rect1: fitz.Rect, rect2: fitz.Rect, tolerance=5):
+    def is_same_line(rect1: fitz.Rect, rect2: fitz.Rect, tolerance=5) -> bool:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def is_rect_inside(
+        outer_rect: fitz.Rect,
+        inner_rect: fitz.Rect,
+        tolerance=5,
+    ) -> bool:
         pass
 
 
 class GeometryUtils(GeometryBaseUtils):
     @staticmethod
-    def is_same_line(rect1: fitz.Rect, rect2: fitz.Rect, tolerance=5):
+    def is_same_line(rect1: fitz.Rect, rect2: fitz.Rect, tolerance=5) -> bool:
         return (
             abs(rect1.y0 - rect2.y0) < tolerance
             or abs(rect1.y1 - rect2.y1) < tolerance
+        )
+
+    @staticmethod
+    def is_rect_inside(
+        outer_rect: fitz.Rect,
+        inner_rect: fitz.Rect,
+        tolerance=5,
+    ) -> bool:
+        return (
+            outer_rect.x0 - tolerance
+            <= inner_rect.x0
+            <= outer_rect.x1 + tolerance
+            and outer_rect.y0 - tolerance
+            <= inner_rect.y0
+            <= outer_rect.y1 + tolerance
+            and outer_rect.x0 - tolerance
+            <= inner_rect.x1
+            <= outer_rect.x1 + tolerance
+            and outer_rect.y0 - tolerance
+            <= inner_rect.y1
+            <= outer_rect.y1 + tolerance
         )
