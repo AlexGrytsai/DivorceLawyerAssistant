3d12c4b61698cb5feb07f90b466f2f1c6f80bab3
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-03 14:40:12 +0200

    refactor: Decouple parser PDF with text, table and widget processors
    This commit refactors the `BaseParserPDF` class to decouple it from specific text, table, and widget processing logic.
    
    Instead of handling these operations directly, the class now accepts `TextBaseProcessor`, `TableBaseProcessor`, and `WidgetBaseProcessor` instances as dependencies.
    
    This change improves the modularity and testability of the code, making it easier to extend and maintain in the future.

--- src/pdf_extractor/parser_pdf.py
+++ src/pdf_extractor/parser_pdf.py
@@ -1,17 +1,28 @@
 from abc import ABC, abstractmethod
-from itertools import chain
-from typing import List, Dict, Any, TypeAlias, Optional
+from typing import List, Dict, Any, TypeAlias
 
 import pymupdf as fitz
 
-from src.pdf_extractor.schemas import SpanPDF, LinePDF, PagePDF, DocumentPDF
+from src.pdf_extractor.schemas import SpanPDF, DocumentPDF
 from src.pdf_extractor.scraper_pdf import ScrapedPage
+from src.pdf_extractor.table_processor import TableBaseProcessor
+from src.pdf_extractor.text_processor import TextBaseProcessor
+from src.pdf_extractor.widget_processor import WidgetBaseProcessor
 
 LineType: TypeAlias = List[Dict[str, Any]] | fitz.Widget
 
 
 class BaseParserPDF(ABC):
-    def __init__(self, scraped_data: List[ScrapedPage]) -> None:
+    def __init__(
+        self,
+        scraped_data: List[ScrapedPage],
+        text_processor: TextBaseProcessor,
+        table_processor: TableBaseProcessor,
+        widget_processor: WidgetBaseProcessor,
+    ) -> None:
+        self._text_processor = text_processor
+        self._table_processor = table_processor
+        self._widget_processor = widget_processor
         self._document = self._prepare_data(scraped_data)
 
     @abstractmethod
@@ -58,133 +69,27 @@ class ParserPDF(BaseParserPDF):
     def _prepare_data(self, scraped_data: List[ScrapedPage]) -> DocumentPDF:
         clean_document = []
         for i in range(len(scraped_data)):
-            page = self.__group_spans_on_page(
+            page = self._text_processor.group_text_on_page(
                 scraped_data[i].text_data, page_number=i + 1
             )
             if scraped_data[i].tables:
                 page.tables = scraped_data[i].tables
 
-                table_lines = self._find_text_lines_in_tables(page)
-                self._update_page_excluding_table_lines(
+                table_lines = self._table_processor.find_text_lines_in_tables(
+                    page
+                )
+                self._table_processor.update_page_excluding_table_lines(
                     table_lines=table_lines,
                     page=page,
                 )
-                self._create_parser_table(table_lines, page)
+                self._table_processor.create_parser_table(table_lines, page)
 
             page.widgets = scraped_data[i].widgets
 
-            self._remove_text_duplicates_with_equal_value_of_the_widget(page)
+            self._text_processor.remove_text_duplicates(page)
 
-            self.__add_widgets_to_lines_on_page(page=page)
+            self._widget_processor.add_widgets_to_lines_on_page(page=page)
 
             clean_document.append(page)
 
         return DocumentPDF(pages=clean_document)
-
-    @staticmethod
-    def __is_same_line(rect1: fitz.Rect, rect2: fitz.Rect, tolerance=5):
-        return (
-            abs(rect1.y0 - rect2.y0) < tolerance
-            or abs(rect1.y1 - rect2.y1) < tolerance
-        )
-
-    @staticmethod
-    def __is_rect_inside(
-        outer_rect: fitz.Rect, inner_rect: fitz.Rect, tolerance=5
-    ):
-        return (
-            outer_rect.x0 - tolerance
-            <= inner_rect.x0
-            <= outer_rect.x1 + tolerance
-            and outer_rect.y0 - tolerance
-            <= inner_rect.y0
-            <= outer_rect.y1 + tolerance
-            and outer_rect.x0 - tolerance
-            <= inner_rect.x1
-            <= outer_rect.x1 + tolerance
-            and outer_rect.y0 - tolerance
-            <= inner_rect.y1
-            <= outer_rect.y1 + tolerance
-        )
-
-    @staticmethod
-    def _update_page_excluding_table_lines(
-        table_lines: List[LinePDF], page: PagePDF
-    ) -> None:
-        table_lines_set = {id(line) for line in table_lines}
-
-        page.lines = [
-            line for line in page.lines if id(line) not in table_lines_set
-        ]
-
-    def _find_text_lines_in_tables(
-        self,
-        page: PagePDF,
-    ) -> List[LinePDF]:
-        return list(
-            filter(
-                lambda line: any(
-                    self.__is_rect_inside(fitz.Rect(table.bbox), line.rect)
-                    for table in page.tables
-                ),
-                chain.from_iterable(page.lines for _ in page.tables),
-            )
-        )
-
-    def _create_parser_table(
-        self,
-        table_lines: List[LinePDF],
-        page: PagePDF,
-    ) -> None:
-        pass
-
-    def __add_widgets_to_lines_on_page(self, page: PagePDF) -> None:
-        for line in page.lines:
-            for widget in page.widgets:
-                if self.__is_same_line(line.text[0].rect, widget.rect):
-                    line.text.append(widget)
-            line.text.sort(key=lambda x: x.rect.x0)
-
-    @staticmethod
-    def _remove_text_duplicates_with_equal_value_of_the_widget(
-        page: PagePDF,
-    ) -> None:
-        widget_value_set = set(
-            widget.field_value for widget in page.widgets if widget.field_value
-        )
-        page.lines = [
-            line
-            for line in page.lines
-            if not any(span.text in widget_value_set for span in line.text)
-        ]
-
-    def __group_spans_on_page(
-        self,
-        spans_from_page: LineType,
-        page_number: Optional[int] = None,
-    ) -> PagePDF | None:
-        if not spans_from_page:
-            return None
-
-        groups_spans_on_page: List[LinePDF] = []
-        spans_on_same_line: List[SpanPDF] = []
-
-        line_rect: fitz.Rect = fitz.Rect(spans_from_page[0].get("bbox"))
-
-        for raw_span in spans_from_page:
-            span = SpanPDF(**raw_span)
-            if self.__is_same_line(line_rect, span.rect):
-                spans_on_same_line.append(span)
-            else:
-                groups_spans_on_page.append(
-                    LinePDF(text=spans_on_same_line, rect=line_rect)
-                )
-                spans_on_same_line = [span]
-                line_rect = span.rect
-
-        if spans_on_same_line:
-            groups_spans_on_page.append(
-                LinePDF(text=spans_on_same_line, rect=line_rect)
-            )
-
-        return PagePDF(page_number=page_number, lines=groups_spans_on_page)
