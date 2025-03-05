151d6fe99c7812b25b01866a662564e6bc5ade7f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-05 12:03:41 +0200

    feat: Refactor text grouping and add span conversion
    This commit refactors the text grouping logic in `TextProcessor` to improve readability and maintainability. It introduces dedicated methods for sorting spans vertically and horizontally, as well as grouping them into lines based on their positions.
    
    Additionally, it adds a static method `convert_raw_spans_to_span_pdf` to convert raw span dictionaries into `SpanPDF` objects, promoting a cleaner and more structured approach to handling span data.

--- src/pdf_extractor/text_processor.py
+++ src/pdf_extractor/text_processor.py
@@ -1,12 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Optional, List, TypeAlias, Dict, Any
+from typing import Optional, List, Dict, Any
 
 import pymupdf as fitz
 
 from src.pdf_extractor.geometry_utils import GeometryBaseUtils
 from src.pdf_extractor.schemas import PagePDF, LinePDF, SpanPDF
-
-LineType: TypeAlias = List[Dict[str, Any]] | fitz.Widget
 
 
 class TextBaseProcessor(ABC):
@@ -15,7 +13,9 @@ class TextBaseProcessor(ABC):
 
     @abstractmethod
     def group_text_on_page(
-        self, spans_from_page: LineType, page_number: Optional[int] = None
+        self,
+        spans_and_widgets_list: List[SpanPDF | fitz.Widget],
+        page_number: Optional[int] = None,
     ) -> PagePDF | None:
         pass
 
@@ -24,25 +24,51 @@ class TextBaseProcessor(ABC):
     def remove_text_duplicates(page: PagePDF) -> None:
         pass
 
+    @staticmethod
+    @abstractmethod
+    def convert_raw_spans_to_span_pdf(
+        raw_span: List[Dict[str, Any]],
+    ) -> List[SpanPDF]:
+        pass
+
 
 class TextProcessor(TextBaseProcessor):
-    def group_text_on_page(
-        self, spans_from_page: LineType, page_number: Optional[int] = None
-    ) -> PagePDF | None:
-        if not spans_from_page:
-            return None
+    @staticmethod
+    def convert_raw_spans_to_span_pdf(
+        raw_span: List[Dict[str, Any]],
+    ) -> List[SpanPDF]:
+        return [SpanPDF(**span) for span in raw_span]
 
+    @staticmethod
+    def _sort_spans_by_vertical_position(
+        spans: List[SpanPDF],
+    ) -> List[SpanPDF]:
+        return sorted(spans, key=lambda y: y.rect.y0)
+
+    @staticmethod
+    def _sort_spans_by_horizontal_position(
+        spans: List[SpanPDF],
+    ) -> List[SpanPDF]:
+        return sorted(spans, key=lambda x: x.rect.x0)
+
+    def _group_spans_into_lines(
+        self, sorted_spans: List[SpanPDF]
+    ) -> List[LinePDF]:
         groups_spans_on_page: List[LinePDF] = []
         spans_on_same_line: List[SpanPDF] = []
-        line_rect: fitz.Rect = fitz.Rect(spans_from_page[0].get("bbox"))
+        line_rect: fitz.Rect = fitz.Rect(sorted_spans[0].rect)
 
-        for raw_span in spans_from_page:
-            span = SpanPDF(**raw_span)
+        for span in sorted_spans:
             if self._geometry_utils.is_same_line(line_rect, span.rect):
                 spans_on_same_line.append(span)
             else:
                 groups_spans_on_page.append(
-                    LinePDF(text=spans_on_same_line, rect=line_rect)
+                    LinePDF(
+                        text=self._sort_spans_by_horizontal_position(
+                            spans_on_same_line
+                        ),
+                        rect=line_rect,
+                    )
                 )
                 spans_on_same_line = [span]
                 line_rect = span.rect
@@ -51,6 +77,23 @@ class TextProcessor(TextBaseProcessor):
             groups_spans_on_page.append(
                 LinePDF(text=spans_on_same_line, rect=line_rect)
             )
+
+        return groups_spans_on_page
+
+    def group_text_on_page(
+        self,
+        spans_and_widgets_list: List[SpanPDF | fitz.Widget],
+        page_number: Optional[int] = None,
+    ) -> Optional[PagePDF]:
+        if not spans_and_widgets_list:
+            return None
+
+        sorted_spans_and_widgets_list = self._sort_spans_by_vertical_position(
+            spans_and_widgets_list
+        )
+        groups_spans_on_page = self._group_spans_into_lines(
+            sorted_spans_and_widgets_list
+        )
 
         return PagePDF(page_number=page_number, lines=groups_spans_on_page)
 
