fc5339961d7f0852449b067bdec7a91a2f780187
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-07 17:12:24 +0200

    feat: Introduce PageFormatter for structured PDF extraction
    This commit introduces the `PageFormatter` class, responsible for converting a `PagePDF` object into a formatted string representation.  It orchestrates the processing of text, tables, and widgets within a page, ensuring proper ordering and formatting.
    
    The `PageFormatter` relies on `TextBaseProcessor`, `TableBaseProcessor`, and `WidgetSpanBaseProces or` for the processing of specific content types. The `format_page` method sorts lines and tables by vertical position and then formats each element accordingly, resulting in a structured and readable string representation of the page's content.

---
+++ src/pdf_extractor/page_formatter.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.pdf_extractor.schemas import PagePDF, TableParsed, LinePDF, SpanPDF
+from src.pdf_extractor.table_processor import TableBaseProcessor
+from src.pdf_extractor.text_processor import TextBaseProcessor
+from src.pdf_extractor.widger_processor import WidgetSpanBaseProcessor
+
+
+class PageFormatterBase(ABC):
+    @abstractmethod
+    def format_page(self, page: PagePDF) -> str:
+        pass
+
+
+class PageFormatter(PageFormatterBase):
+    __slots__ = ("_text_processor", "_table_processor", "_widget_processor")
+
+    def __init__(
+        self,
+        text_processor: TextBaseProcessor,
+        table_processor: TableBaseProcessor,
+        widget_processor: WidgetSpanBaseProcessor,
+    ) -> None:
+        self._text_processor = text_processor
+        self._table_processor = table_processor
+        self._widget_processor = widget_processor
+
+    def format_page(self, page: PagePDF) -> str:
+        result = [f"Page # {page.page_number}\n\n"]
+        elements = self._text_processor.sort_spans_by_vertical_position(
+            page.lines + page.parsed_tables
+        )
+        element: List[LinePDF | TableParsed]
+        for element in elements:
+            if isinstance(element, TableParsed):
+                result.append(
+                    self._table_processor.format_table_to_string(element)
+                )
+            else:
+                processed_text = self._process_text_line(element)
+                result.append(processed_text)
+
+        return "\n".join(result) + "\n"
+
+    def _process_text_line(self, line: LinePDF) -> str:
+        checked_line = self._widget_processor.handle_widget_span_intersections(
+            line
+        )
+        return " ".join(
+            (
+                span.text
+                if isinstance(span, SpanPDF)
+                else f"[{self._widget_processor.get_widget_value(span)}]"
+            )
+            for span in checked_line.text
+        )
