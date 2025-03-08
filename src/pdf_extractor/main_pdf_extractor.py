1a0ad9a90bfb72c5c12799a38195407b57bdc760
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-08 13:37:35 +0200

    feat: Add main PDF extractor module
    This commit introduces the main PDF extractor module.
    This module orchestrates the extraction of text, tables, and widgets from PDF documents using various processors and utility classes. It prepares the extracted data for AI processing and returns the document as plain text.

---
+++ src/pdf_extractor/main_pdf_extractor.py
@@ -0,0 +1,36 @@
+from src.pdf_extractor.geometry_utils import GeometryUtils
+from src.pdf_extractor.page_formatter import PageFormatter
+from src.pdf_extractor.parser_pdf import ParserPDF
+from src.pdf_extractor.scraper_pdf import ScraperPDF
+from src.pdf_extractor.table_processor import TableProcessor
+from src.pdf_extractor.text_processor import TextProcessor
+from src.pdf_extractor.widger_processor import WidgetSpanProcessor
+
+
+def main_pdf_extractor(path_to_pdf: str) -> str:
+    scrap_pdf = ScraperPDF(path_to_pdf)
+
+    geometric_utils = GeometryUtils()
+
+    text_processor = TextProcessor(geometry_utils=geometric_utils)
+    table_processor = TableProcessor(geometry_utils=geometric_utils)
+    widget_processor = WidgetSpanProcessor(
+        geometry_utils=geometric_utils,
+        text_processor=text_processor,
+    )
+
+    page_formatter = PageFormatter(
+        text_processor=text_processor,
+        table_processor=table_processor,
+        widget_processor=widget_processor,
+    )
+
+    parser = ParserPDF(
+        text_processor=text_processor,
+        table_processor=TableProcessor(geometry_utils=geometric_utils),
+        page_formatter=page_formatter,
+    )
+
+    parser.prepare_data(scrap_pdf.scrap_data(), is_for_ai=True)
+
+    return parser.document_as_text
