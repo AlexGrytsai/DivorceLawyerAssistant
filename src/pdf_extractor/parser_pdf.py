ffcefa60f1ffa9af012c193979c4852b5b8a0001
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-08 17:18:23 +0200

    feat: Add widget processing to PDF parser
    This commit introduces widget processing to the PDF parser.
    It adds a `WidgetSpanBaseProcessor` dependency and extracts text from widgets using it. The extracted widget data is stored in the `_widget_data` attribute as a dictionary.
    Two abstract methods are defined and implemented to allow access to the string and widget data.

--- src/pdf_extractor/parser_pdf.py
+++ src/pdf_extractor/parser_pdf.py
@@ -8,6 +8,7 @@ from src.pdf_extractor.schemas import DocumentPDF
 from src.pdf_extractor.scraper_pdf import ScrapedPage
 from src.pdf_extractor.table_processor import TableBaseProcessor
 from src.pdf_extractor.text_processor import TextBaseProcessor
+from src.pdf_extractor.widger_processor import WidgetSpanBaseProcessor
 
 LineType: TypeAlias = List[Dict[str, Any]] | fitz.Widget
 
@@ -20,7 +21,12 @@ class BaseParserPDF(ABC):
 
     @property
     @abstractmethod
-    def document_as_text(self) -> Optional[str]:
+    def document_as_text(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def widget_data_dict(self) -> Dict[str, str]:
         pass
 
 
@@ -28,24 +34,33 @@ class ParserPDF(BaseParserPDF):
     __slots__ = (
         "_text_processor",
         "_table_processor",
+        "_widget_processor",
         "_page_formatter",
         "_document_as_str",
+        "_widget_data",
     )
 
     def __init__(
         self,
         text_processor: TextBaseProcessor,
         table_processor: TableBaseProcessor,
+        widget_processor: WidgetSpanBaseProcessor,
         page_formatter: PageFormatterBase,
     ) -> None:
         self._text_processor = text_processor
         self._table_processor = table_processor
+        self._widget_processor = widget_processor
         self._page_formatter = page_formatter
-        self._document_as_str: Optional[str] = None
+        self._document_as_str: str = ""
+        self._widget_data: Dict[str, str] = {}
 
     @property
-    def document_as_text(self) -> Optional[str]:
+    def document_as_text(self) -> str:
         return self._document_as_str
+
+    @property
+    def widget_data_dict(self) -> Dict[str, str]:
+        return self._widget_data
 
     def _convert_document_to_string(
         self,
@@ -67,4 +82,7 @@ class ParserPDF(BaseParserPDF):
 
         self._document_as_str = self._convert_document_to_string(
             DocumentPDF(pages=pages), is_for_ai=is_for_ai
+        )
+        self._widget_data = self._widget_processor.extract_text_widgets(
+            [widget for page in pages for widget in page.widgets]
         )
