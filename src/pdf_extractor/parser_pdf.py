e9d683c369165e7018150b64f01d9b88a10d7523
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-08 06:54:32 +0200

    refactor: Remove unused imports and types
    This commit removes unused imports and type aliases from the `parser_pdf.py` file. Specifically, `SpanPDF`, `TableParsed`, and `WidgetSpanBaseProcessor` are no longer used and have been removed to clean up the codebase. The `PageFormatter` import has also been removed, as only the base class is used. This improves code readability and reduces potential confusion.

--- src/pdf_extractor/parser_pdf.py
+++ src/pdf_extractor/parser_pdf.py
@@ -3,12 +3,11 @@ from typing import List, Dict, Any, TypeAlias
 
 import pymupdf as fitz  # type: ignore
 
-from src.pdf_extractor.page_formatter import PageFormatter, PageFormatterBase
-from src.pdf_extractor.schemas import SpanPDF, DocumentPDF, TableParsed
+from src.pdf_extractor.page_formatter import PageFormatterBase
+from src.pdf_extractor.schemas import DocumentPDF
 from src.pdf_extractor.scraper_pdf import ScrapedPage
 from src.pdf_extractor.table_processor import TableBaseProcessor
 from src.pdf_extractor.text_processor import TextBaseProcessor
-from src.pdf_extractor.widger_processor import WidgetSpanBaseProcessor
 
 LineType: TypeAlias = List[Dict[str, Any]] | fitz.Widget
 
