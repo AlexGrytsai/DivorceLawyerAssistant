384673609dc39cbf2dbebb45d681f3ceed683624
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-08 17:17:39 +0200

    feat: Update OpenAI model configuration and add token limits
    This commit updates the way OpenAI API key and model are accessed, using `os.environ` instead of `os.getenv` to ensure they are set.
    It also introduces token limits for the new gpt-4o model and gpt-3.5-turbo-16k model and fixes the default token limit retrieval to return 0 if the model is not found.

--- src/core/config.py
+++ src/core/config.py
@@ -19,21 +19,24 @@ redis_client_for_performance_monitoring = Redis(
 class Settings:
     DEBUG: bool = True
 
-    OPENAI_MODEL: str = os.getenv("OPENAI_MODEL")
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL: str = os.environ["OPENAI_MODEL"]
+    OPENAI_API_KEY: str = os.environ["OPENAI_MODEL"]
 
     _MODEL_TOKEN_LIMITS = {
         "gpt-4": 10000,
+        "gpt-4o": 30000,
+        "gpt-4o-2024-08-06": 30000,
         "gpt-4o-realtime-preview": 20000,
         "gpt-4-turbo": 30000,
         "gpt-3.5-turbo": 200000,
+        "gpt-3.5-turbo-16k": 160000,
     }
 
     MODEL_TOKEN_LIMITS = MappingProxyType(_MODEL_TOKEN_LIMITS)
 
     @property
     def get_token_limit(self) -> int:
-        return self.MODEL_TOKEN_LIMITS.get(self.OPENAI_MODEL, None)
+        return self.MODEL_TOKEN_LIMITS.get(self.OPENAI_MODEL, 0)
 
 
 LOGGING_CONFIG = {
