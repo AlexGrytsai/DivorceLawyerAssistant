ecbc1a700b4f8b610e94f4e735f676ceb97fb597
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-11 18:00:20 +0200

    feat: Expose additional modules in pdf_extractor package
    This commit exposes several modules within the `pdf_extractor` package, including `PageFormatter`, `GeometryUtils`, `ParserPDF`, `BaseParserPDF`, `TableProcessor`, `TextProcessor`, and `WidgetSpanProcessor`. This change allows other parts of the application to directly import and utilize these components when needed, enhancing modularity and flexibility.

--- src/services/pdf_extractor/__init__.py
+++ src/services/pdf_extractor/__init__.py
@@ -1,3 +1,18 @@
 from .pdf_extractor import extract_pdf_data
+from .page_formatter import PageFormatter
+from .geometry_utils import GeometryUtils
+from .parser_pdf import ParserPDF, BaseParserPDF
+from .table_processor import TableProcessor
+from .text_processor import TextProcessor
+from .widger_processor import WidgetSpanProcessor
 
-__all__ = ["extract_pdf_data"]
+__all__ = [
+    "extract_pdf_data",
+    "PageFormatter",
+    "GeometryUtils",
+    "ParserPDF",
+    "BaseParserPDF",
+    "TableProcessor",
+    "TextProcessor",
+    "WidgetSpanProcessor",
+]
