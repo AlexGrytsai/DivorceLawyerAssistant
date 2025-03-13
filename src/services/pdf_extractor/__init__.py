9e6b451afa08497c6812a925512f6c5a45de47dc
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-13 08:11:53 +0200

    refactor: Reorganize and standardize imports in pdf_extractor
    This commit refactors the `__init__.py` file in the `pdf_extractor` service. It standardizes and organizes the imports and the `__all__` list for better readability and maintainability. It also introduces Base classes to be exposed.
    
    The changes ensure that all necessary modules are correctly imported and publicly available, which improves the overall structure of the package.

--- src/services/pdf_extractor/__init__.py
+++ src/services/pdf_extractor/__init__.py
@@ -1,18 +1,29 @@
-from .pdf_extractor import extract_pdf_data
-from .page_formatter import PageFormatter
+from .geometry_utils import GeometryBaseUtils
 from .geometry_utils import GeometryUtils
-from .parser_pdf import ParserPDF, BaseParserPDF
+from .page_formatter import PageFormatter
+from .page_formatter import PageFormatterBase
+from .parser_pdf import ParserPDFBase
+from .parser_pdf import ParserPDF
+from .pdf_extractor import extract_pdf_data
+from .table_processor import TableBaseProcessor
 from .table_processor import TableProcessor
+from .text_processor import TextBaseProcessor
 from .text_processor import TextProcessor
+from .widger_processor import WidgetSpanBaseProcessor
 from .widger_processor import WidgetSpanProcessor
 
 __all__ = [
     "extract_pdf_data",
+    "PageFormatterBase",
     "PageFormatter",
+    "GeometryBaseUtils",
     "GeometryUtils",
+    "ParserPDFBase",
     "ParserPDF",
-    "BaseParserPDF",
+    "TableBaseProcessor",
     "TableProcessor",
+    "TextBaseProcessor",
     "TextProcessor",
+    "WidgetSpanBaseProcessor",
     "WidgetSpanProcessor",
 ]
