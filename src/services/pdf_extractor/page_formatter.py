6d3e46460b646c1bb528079c476e9cc6b0ea1262
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-13 08:08:53 +0200

    refactor: Refactor PageFormatterBase to use widget labels
    This commit refactors the `PageFormatterBase` class to use widget labels instead of a boolean flag for AI usage.
    
    The `format_page` method now accepts a `use_widget_label` argument, which determines whether or not to include widget labels in the formatted page output. This simplifies the logic and makes the code more readable.
    
    This change is necessary to improve the flexibility of the page formatting process.

--- src/services/pdf_extractor/page_formatter.py
+++ src/services/pdf_extractor/page_formatter.py
@@ -1,15 +1,24 @@
 from abc import ABC, abstractmethod
 from typing import List, Union
 
-from src.services.pdf_extractor.schemas import PagePDF, TableParsed, LinePDF, SpanPDF
-from src.services.pdf_extractor.table_processor import TableBaseProcessor
-from src.services.pdf_extractor.text_processor import TextBaseProcessor
-from src.services.pdf_extractor.widger_processor import WidgetSpanBaseProcessor
+from src.services.pdf_extractor import (
+    TextBaseProcessor,
+    TableBaseProcessor,
+    WidgetSpanBaseProcessor,
+)
+from src.services.pdf_extractor.schemas import (
+    PagePDF,
+    TableParsed,
+    LinePDF,
+    SpanPDF,
+)
 
 
 class PageFormatterBase(ABC):
     @abstractmethod
-    def format_page(self, page: PagePDF, is_for_ai: bool = True) -> str:
+    def format_page(
+        self, page: PagePDF, use_widget_label: bool = False
+    ) -> str:
         pass
 
 
@@ -26,7 +35,11 @@ class PageFormatter(PageFormatterBase):
         self._table_processor = table_processor
         self._widget_processor = widget_processor
 
-    def format_page(self, page: PagePDF, is_for_ai: bool = True) -> str:
+    def format_page(
+        self,
+        page: PagePDF,
+        use_widget_label: bool = False,
+    ) -> str:
         result = [f"Page # {page.page_number}\n"]
 
         liens: List[LinePDF] = page.lines if page.lines else []
@@ -40,7 +53,7 @@ class PageFormatter(PageFormatterBase):
         element: Union[LinePDF, TableParsed]
         for element in elements:  # type: ignore[assignment]
             if isinstance(element, TableParsed):
-                if is_for_ai:
+                if use_widget_label:
 
                     result.append(
                         self._table_processor.format_table_to_string_for_ai(
