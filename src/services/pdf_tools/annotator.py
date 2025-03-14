a4801eae09d89a3e405bdccbd3f7ffe2fa28450d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-14 17:19:29 +0200

    feat: Add function for comment positioning and refactor annotation logic
    This commit introduces a `get_comment_position` function to encapsulate the logic for determining the position of comment annotations. It enhances code readability and maintainability by abstracting away the calculation of annotation coordinates.
    
    The `add_comments_to_widgets` function is refactored to use this new function, making the code cleaner and easier to understand. The change ensures consistent placement of comments relative to the widget within the PDF.

--- src/services/pdf_tools/annotator.py
+++ src/services/pdf_tools/annotator.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict
+from typing import Dict, Any
 
 import pymupdf as fitz
 
@@ -9,21 +9,24 @@ def create_new_file_name(pdf_path: str) -> str:
     return f"{file_name}_checked{extension}"
 
 
+def get_comment_position(
+    page_width: int, widget: fitz.Widget
+) -> tuple[int, int]:
+    return page_width - 50, widget.rect.y0
+
+
 def add_comments_to_widgets(
     pdf_path: str,
     comments: Dict[str, str],
 ) -> None:
     doc = fitz.open(pdf_path)
     for page in doc:
-        page_width = page.rect.width
         for widget in page.widgets():
             if widget.field_name in comments:
-                widget_mid_y = widget.rect.y0
-                annot_x = page_width - 50
-                annot_y = widget_mid_y
-                comment_text = comments[widget.field_name]
                 page.add_text_annot(
-                    (annot_x, annot_y), comment_text, icon="Note"
+                    get_comment_position(page.rect.width, widget),
+                    comments[widget.field_name],
+                    icon="Note",
                 )
 
     doc.save(create_new_file_name(pdf_path))
