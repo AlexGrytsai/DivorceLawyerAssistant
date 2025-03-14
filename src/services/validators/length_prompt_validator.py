65c8d048403a5fae0330269ecb093625388e4ea8
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-14 09:55:18 +0200

    refactor: Rename validator function and add get length function
    This commit renames the `get_length_prompt` function to `is_length_prompt_valid` to better reflect its boolean return value indicating validity. A new function `get_length_prompt` is added to return the actual token length of the prompt. This change improves clarity and provides more functionality.

--- src/services/validators/length_prompt_validator.py
+++ src/services/validators/length_prompt_validator.py
@@ -3,7 +3,7 @@ import tiktoken
 from src.core import settings
 
 
-def get_length_prompt(prompt: str) -> bool:
+def is_length_prompt_valid(prompt: str) -> bool:
     if (
         len(tiktoken.encoding_for_model(settings.BASE_AI_MODEL).encode(prompt))
         > settings.MODEL_TOKEN_LIMITS
@@ -11,3 +11,9 @@ def get_length_prompt(prompt: str) -> bool:
         return False
 
     return True
+
+
+def get_length_prompt(prompt: str) -> int:
+    return len(
+        tiktoken.encoding_for_model(settings.BASE_AI_MODEL).encode(prompt)
+    )
