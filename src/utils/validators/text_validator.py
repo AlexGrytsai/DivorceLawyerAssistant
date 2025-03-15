8a8ba994ee6ef6155f48b4ed63e0eaba8a63262d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-15 13:13:55 +0200

    feat: Refactor widget validation and add error aggregation
    The commit refactors the widget validation process to improve accuracy and error reporting. It introduces the use of `pymupdf` to check text length against widget width and aggregates multiple errors for a single widget. The changes include:
    
    - Replacing line length validation with widget width validation.
    - Adding error aggregation for widgets.
    - Removing unused max length constants.
    - Modifying the `validate_widgets` to handle widget instances.
    - Adding null check for widgets in AI validation
    
    These changes enhance the accuracy of the validation and provide more comprehensive feedback to the user.

--- src/utils/validators/text_validator.py
+++ src/utils/validators/text_validator.py
@@ -2,8 +2,9 @@ import json
 import re
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 
+import pymupdf as fitz
 from email_validator import validate_email, EmailNotValidError
 
 from src.services.ai_service.ai_text_validator import (
@@ -17,7 +18,7 @@ class TextBaseValidator(ABC):
     @staticmethod
     @abstractmethod
     async def validate_line_length(
-        line: str, max_length: int
+        widget_instance: fitz.Widget,
     ) -> Tuple[bool, str]:
         pass
 
@@ -49,19 +50,20 @@ class TextBaseValidator(ABC):
 
 
 class TextWidgetValidatorUseAI(TextBaseValidator):
-    MAX_LENGTH_IN_TABLE = 70
-    MAX_LENGTH_IN_TEXT = 120
-
     def __init__(self, ai_assistant: OpenAITextAnalyzer) -> None:
         self._ai_assistant = ai_assistant
+        self._errors_in_widgets = {}
 
     @staticmethod
     async def validate_line_length(
-        line: str, max_length: int
+        widget_instance: fitz.Widget,
     ) -> Tuple[bool, str]:
-        if len(line) <= max_length:
-            return True, ""
-        return False, "Line length is too long"
+        widget_value_char_width = fitz.Font("Helvetica").text_length(
+            widget_instance.field_value, widget_instance.text_fontsize
+        )
+        if widget_value_char_width > widget_instance.rect.width:
+            return False, "Line length is too long"
+        return True, ""
 
     @staticmethod
     async def is_caps_lock_on(line: str) -> Tuple[bool, str]:
@@ -107,35 +109,42 @@ class TextWidgetValidatorUseAI(TextBaseValidator):
         return False, "Phone number is not valid"
 
     async def validate_widgets(
-        self, widgets: Dict[str, Dict[str, str]]
+        self, widgets: Dict[str, Dict[str, Union[str, fitz.Widget]]]
     ) -> Dict[str, str]:
         errors_in_widgets = {}
         widgets_for_ai = {}
-        for place_of_widget, value in widgets.items():
-            for widget_name, widget_value in value.items():
+        for widget_name, values_dict in widgets.items():
+            if values_dict.get("value"):
+                widget_value, widget_instance = (
+                    values_dict["value"],
+                    values_dict["widget_instance"],
+                )
                 if "email" in widget_name:
                     is_valid_email, error_message = await self.email_validator(
                         widget_value
                     )
                     if not is_valid_email:
-                        errors_in_widgets[widget_name] = error_message
+                        await self._add_error_to_dict(
+                            widget_name, error_message
+                        )
                 else:
                     is_valid_length, error_message = (
-                        await self.validate_line_length(
-                            widget_value,
-                            self._widget_max_length(place_of_widget),
-                        )
+                        await self.validate_line_length(widget_instance)
                     )
                     if not is_valid_length:
-                        errors_in_widgets[widget_name] = error_message
+                        await self._add_error_to_dict(
+                            widget_name, error_message
+                        )
 
                     is_caps_locked, caps_message = await self.is_caps_lock_on(
                         widget_value
                     )
                     if is_caps_locked:
-                        errors_in_widgets[widget_name] = caps_message
-                    elif is_valid_length and not is_caps_locked:
-                        widgets_for_ai[widget_name] = widget_value
+                        await self._add_error_to_dict(
+                            widget_name, caps_message
+                        )
+
+                    widgets_for_ai[widget_name] = widget_value
 
         address_dates_phones = await self._check_widget_data_with_ai(
             widgets=widgets_for_ai,
@@ -169,20 +178,25 @@ class TextWidgetValidatorUseAI(TextBaseValidator):
         ai_assistant: AIBaseValidator,
         assistant_prompt: str,
     ) -> Dict[str, Dict[str, str]]:
-        prompt = (
-            f"Analyze the following JSON dictionary:\n"
-            f"{json.dumps(widgets)}"
-        )
-        address_dates_phones = await ai_assistant.analyze_text(
-            prompt=prompt,
-            assistant_prompt=assistant_prompt,
-        )
+        if widgets:
+            prompt = (
+                f"Analyze the following JSON dictionary:\n"
+                f"{json.dumps(widgets)}"
+            )
+            address_dates_phones = await ai_assistant.analyze_text(
+                prompt=prompt,
+                assistant_prompt=assistant_prompt,
+            )
+            return address_dates_phones
 
-        return address_dates_phones
+        return {}
 
-    def _widget_max_length(self, place_of_widget: str) -> int:
-        if place_of_widget == "Table":
-            return self.MAX_LENGTH_IN_TABLE
-        elif place_of_widget == "Text":
-            return self.MAX_LENGTH_IN_TEXT
-        raise ValueError(f"Unknown widget type: {place_of_widget}")
+    async def _add_error_to_dict(
+        self,
+        widget_name: str,
+        error_message: str,
+    ) -> None:
+        if not self._errors_in_widgets.get(widget_name):
+            self._errors_in_widgets[widget_name] = error_message
+        else:
+            self._errors_in_widgets[widget_name] += f"\n{error_message}"
