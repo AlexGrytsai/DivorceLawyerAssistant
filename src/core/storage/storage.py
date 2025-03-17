bf3b00a1a013ac9d706da097e205a2e08fbce8d2
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-17 17:08:32 +0200

    feat: Implement local storage functionality
    This commit introduces a local storage module, allowing the application to save uploaded files to a specified directory.
    
    The `BaseStorage` class defines the abstract interface for storage operations.  The `LocalStorage` class implements this interface, providing methods for uploading single and multiple files.
    
    This change provides a foundation for handling file uploads and
    storage within the application.

---
+++ src/core/storage/storage.py
@@ -0,0 +1,88 @@
+import asyncio
+import logging
+from abc import ABC, abstractmethod
+from typing import List, Optional, Union
+
+from fastapi import UploadFile
+
+from core.storage.exceptions import ErrorSavingFile, ErrorUploadingFile
+from core.storage.shemas import FileData
+
+logger = logging.getLogger(__name__)
+
+
+class BaseStorage(ABC):
+    async def __call__(
+        self,
+        file: Optional[UploadFile] = None,
+        files: Optional[List[UploadFile]] = None,
+    ) -> Union[FileData, List[FileData]]:
+        try:
+            if file:
+                return await self.upload(file=file)
+
+            elif files:
+                return await self.multi_upload(files=files)
+            else:
+                logger.warning("No file or files provided")
+                return FileData(
+                    status=False,
+                    error="No file or files provided",
+                    message="No file or files provided",
+                )
+        except ErrorSavingFile as exc:
+            logger.warning(f"Error saving file: {exc}")
+            return FileData(
+                status=False,
+                error=str(exc),
+                message="File upload was unsuccessful",
+            )
+
+    @abstractmethod
+    async def upload(self, *args, file: UploadFile) -> FileData:
+        pass
+
+    @abstractmethod
+    async def multi_upload(
+        self, *args, files: List[UploadFile]
+    ) -> List[FileData]:
+        pass
+
+
+class LocalStorage(BaseStorage):
+    __slots__ = ("_path_to_storage",)
+
+    def __init__(self, path_to_storage: str) -> None:
+        self._path_to_storage = path_to_storage
+
+    async def upload(self, *args, file: UploadFile) -> FileData:
+        try:
+            file_object = await file.read()
+
+            with open(f"{self._path_to_storage}", "wb") as fh:
+                fh.write(file_object)
+            await file.close()
+
+            return FileData(
+                path=self._path_to_storage,
+                message=f"{file.filename} saved successfully",
+                content_type=file.content_type,
+                size=file.size,
+                filename=file.filename,
+            )
+        except ErrorUploadingFile as exc:
+            logger.warning(
+                f"Error uploading file: {exc} in {self.__class__.__name__}"
+            )
+
+            return FileData(
+                status=False, error=str(exc), message=f"Unable to save file"
+            )
+
+    async def multi_upload(
+        self, *args, files: List[UploadFile]
+    ) -> List[FileData]:
+        uploaded = await asyncio.gather(
+            *[self.upload(file=file) for file in files]
+        )
+        return list(uploaded)
