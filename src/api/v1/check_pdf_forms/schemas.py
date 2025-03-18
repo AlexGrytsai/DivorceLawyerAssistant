86a652dd25b3709cf86c7caeee08dd6974503d6b
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-18 13:45:06 +0200

    refactor: Rename CheckedForm to FileDataSchema
    This commit renames the `CheckedForm` Pydantic model to `FileDataSchema`. This renaming provides a more accurate and descriptive name for the schema, improving code clarity and maintainability. The schema represents data about a file, so `FileDataSchema` better reflects its purpose. No breaking changes are introduced.

--- src/api/v1/check_pdf_forms/schemas.py
+++ src/api/v1/check_pdf_forms/schemas.py
@@ -3,16 +3,7 @@ from pathlib import Path
 from pydantic import HttpUrl, BaseModel
 
 
-class FileDataUpload(BaseModel):
-    path: Path | str
-    size: int = 0
-    filename: str
-    content_type: str
-    status_code: int
-    message: str
-
-
-class CheckedForm(BaseModel):
+class FileDataSchema(BaseModel):
     path: Path | str
     url: HttpUrl | str
     size: int = 0
