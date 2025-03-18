ac34de795872d69b7d210106be92120485c2f814
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-18 13:47:40 +0200

    feat: Enhance file storage with URL generation and error handling
    This commit enhances the file storage functionality by:
    
    - Returning the saved file's URL in the response - Raising HTTPExceptions instead of returning JSONResponse directly, for better integration with FastAPI's error handling - Includes status code in the response when a file is successfully saved - Corrected error messages for the exceptions.

--- src/core/storage/storage.py
+++ src/core/storage/storage.py
@@ -1,15 +1,18 @@
 import asyncio
 import logging
 import os
+import urllib
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import List, Optional, Union
 
-from fastapi import UploadFile, status
+from fastapi import UploadFile, status, HTTPException, Request
 from starlette.responses import JSONResponse
 
+from src.api.v1.check_pdf_forms.schemas import (
+    FileDataSchema,
+)
 from src.core.storage.exceptions import ErrorSavingFile, ErrorUploadingFile
-from src.core.storage.shemas import FileData
 
 logger = logging.getLogger(__name__)
 
@@ -19,40 +22,42 @@ class BaseStorage(ABC):
         self,
         file: Optional[UploadFile] = None,
         files: Optional[List[UploadFile]] = None,
-    ) -> Union[FileData, List[FileData], JSONResponse]:
+        request: Request = None,
+    ) -> Union[FileDataSchema, List[FileDataSchema]]:
         try:
             if file:
-                return await self.upload(file=file)
+                return await self.upload(file=file, request=request)
 
             elif files:
-                return await self.multi_upload(files=files)
+                return await self.multi_upload(files=files, request=request)
 
             logger.warning("No file or files provided")
-            return JSONResponse(
-                content={
-                    "error": "No file or files provided",
-                    "message": "No file or files provided",
-                },
+
+            raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No file or files provided",
             )
         except ErrorSavingFile as exc:
             logger.warning(f"Error saving file: {exc}")
-            return JSONResponse(
-                content={
+
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={
                     "error": str(exc),
                     "message": "File upload was unsuccessful",
                 },
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
     @abstractmethod
-    async def upload(self, file: UploadFile, *args, **kwargs) -> FileData:
+    async def upload(
+        self, file: UploadFile, request: Request, *args, **kwargs
+    ) -> FileDataSchema:
         pass
 
     @abstractmethod
     async def multi_upload(
-        self, *args, files: List[UploadFile]
-    ) -> List[FileData]:
+        self, files: List[UploadFile], request: Request, *args, **kwargs
+    ) -> List[FileDataSchema]:
         pass
 
     @abstractmethod
@@ -66,40 +71,58 @@ class LocalStorage(BaseStorage):
     def __init__(self, path_to_storage: str) -> None:
         self._path_to_storage = path_to_storage
 
-    async def upload(self, file: UploadFile, *args, **kwargs) -> FileData:
+    async def upload(
+        self,
+        file: UploadFile,
+        request: Request,
+        *args,
+        **kwargs,
+    ) -> FileDataSchema:
         try:
             file_object = await file.read()
 
             storage_path = Path(self._path_to_storage)
             storage_path.mkdir(parents=True, exist_ok=True)
 
-            file_path = Path(self._path_to_storage) / file.filename
+            file_path = os.path.join(storage_path, file.filename)
 
             with open(file_path, "wb") as fh:
                 fh.write(file_object)
             await file.close()
 
-            return FileData(
-                path=f"{self._path_to_storage}/{file.filename}",
+            base_url = str(request.base_url).rstrip("/")
+            url_path = urllib.parse.quote(file_path.replace(os.sep, "/"))
+
+            return FileDataSchema(
+                path=file_path,
+                url=f"{base_url}/{url_path}",
                 message=f"{file.filename} saved successfully",
                 content_type=file.content_type,
                 size=file.size,
                 filename=file.filename,
+                status_code=status.HTTP_201_CREATED,
             )
         except ErrorUploadingFile as exc:
             logger.warning(
                 f"Error uploading file: {exc} in {self.__class__.__name__}"
             )
-
-            return FileData(
-                status=False, error=str(exc), message=f"Unable to save file"
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "error": str(exc),
+                    "message": "File upload was unsuccessful",
+                },
             )
 
     async def multi_upload(
-        self, *args, files: List[UploadFile]
-    ) -> List[FileData]:
+        self,
+        files: List[UploadFile],
+        request: Request,
+        *args,
+        **kwargs,
+    ) -> List[FileDataSchema]:
         uploaded = await asyncio.gather(
-            *[self.upload(file=file) for file in files]
+            *[self.upload(file=file, request=request) for file in files]
         )
         return list(uploaded)
 
@@ -113,27 +136,27 @@ class LocalStorage(BaseStorage):
 
                 return JSONResponse(
                     content={"message": f"{filename} deleted successfully"},
-                    status_code=status.HTTP_200_OK,
+                    status_code=status.HTTP_204_NO_CONTENT,
                 )
             else:
                 logger.warning(
                     f"{filename} not found in {self._path_to_storage} "
                     f"for deletion"
                 )
-                return JSONResponse(
-                    content={
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail={
                         "error": "File not found",
                         "message": f"{filename} not found",
                     },
-                    status_code=status.HTTP_404_NOT_FOUND,
                 )
 
         except Exception as exc:
             logger.error(f"Error deleting file {filename}: {exc}")
-            return JSONResponse(
-                content={
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={
                     "error": str(exc),
                     "message": f"Error deleting {filename}",
                 },
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
