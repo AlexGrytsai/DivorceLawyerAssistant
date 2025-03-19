b8891aa5ec127f6ff84844bf59f5e4524f83d3da
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-19 10:17:14 +0200

    feat: Add decorator for handling file deletion exceptions
    This commit introduces a new decorator, `handle_delete_file_exceptions`, to gracefully handle exceptions that may occur during file deletion operations. It logs the error and raises an HTTP exception with a 500 status code, providing more informative error messages to the client. This enhances the robustness and user experience of file deletion functionality. It also imports ErrorDeletingFile.

--- src/core/storage/decorators.py
+++ src/core/storage/decorators.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 from fastapi import HTTPException, status
 
-from src.core.storage.exceptions import ErrorUploadingFile
+from src.core.storage.exceptions import ErrorUploadingFile, ErrorDeletingFile
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +24,28 @@ def handle_upload_file_exceptions(func: Callable) -> Callable:
                 detail={
                     "error": str(exc),
                     "message": "File upload was unsuccessful",
+                },
+            )
+
+    return wrapper
+
+
+def handle_delete_file_exceptions(func: Callable) -> Callable:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            result = await func(*args, **kwargs)
+            return result
+        except ErrorDeletingFile as exc:
+            file_path = kwargs.get(
+                "file_path", "file_path not transferred as a named argument"
+            )
+            logger.error(f"Error deleting file {file_path}: {exc}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={
+                    "error": str(exc),
+                    "message": f"Error deleting {file_path}",
                 },
             )
 
