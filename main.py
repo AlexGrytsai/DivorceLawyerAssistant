6921dd17085d96954d66e2ff37fc9724c3541f7a
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-20 06:54:28 +0200

    feat: Refactor main.py to FastAPI application
    This commit refactors the original `main.py` file to set up a FastAPI application. It includes:
    
    - Importing necessary modules for FastAPI and static files.
    - Initializing the FastAPI application.
    - Including the router for PDF checking functionality.
    - Mounting a static directory for serving files.
    
    This change sets the foundation for a web-based UI for the PDF forms checker. The previous version was only for console usage.

--- main.py
+++ main.py
@@ -1,52 +1,19 @@
-import asyncio
-from typing import Type, List
+import logging
 
-from openai import AsyncOpenAI
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+from src.api.v1.check_pdf_forms.router import router_pdf_check
 from src.core import settings
-from src.core.config import redis_client_for_performance_monitoring
-from src.services.ai_service.ai_text_validator import OpenAITextAnalyzer
-from src.services.check_pdf_fields import main_check_pdf_fields
-from src.services.pdf_tools.parser_pdf import (
-    ParserPDFBase,
-    ParserPDFWidget,
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+app.include_router(router_pdf_check)
+
+app.mount(
+    f"/{settings.STATIC_DIR}",
+    StaticFiles(directory=settings.STATIC_DIR),
+    name="uploads",
 )
-from src.utils.performance_monitoring.cpu import cpu_monitor_decorator
-from src.utils.performance_monitoring.ram import ram_monitor_decorator
-from src.utils.validators.text_validator import (
-    TextWidgetValidatorUseAI,
-)
-
-
-@cpu_monitor_decorator(r_client=redis_client_for_performance_monitoring)
-@ram_monitor_decorator(r_client=redis_client_for_performance_monitoring)
-async def main(
-    path_to_pdf: List[str],
-    parser_type: Type[ParserPDFBase],
-    validator_type: Type[TextWidgetValidatorUseAI],
-    assistant_instance: OpenAITextAnalyzer,
-) -> None:
-    await main_check_pdf_fields(
-        paths_to_pdf=path_to_pdf,
-        widget_parser_type=parser_type,
-        validator_type=validator_type,
-        ai_assistant=assistant_instance,
-    )
-
-
-if __name__ == "__main__":
-    ai_assistant = OpenAITextAnalyzer(
-        openai_client=AsyncOpenAI(
-            api_key=settings.OPENAI_API_KEY, max_retries=5
-        ),
-        openai_model=settings.BASE_AI_MODEL,
-    )
-
-    asyncio.run(
-        main(
-            path_to_pdf=["test_pdf.pdf", "document mi.pdf"],
-            parser_type=ParserPDFWidget,
-            validator_type=TextWidgetValidatorUseAI,
-            assistant_instance=ai_assistant,
-        )
-    )
