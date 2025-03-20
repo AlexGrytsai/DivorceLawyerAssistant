b1f977f9ba5ff8cb9edb11011dc1bbcf0b6c7838
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-20 08:02:34 +0200

    feat: Serve a basic HTML UI using Jinja2 templates
    This commit introduces a basic HTML UI served using Jinja2 templates.
    The UI is served at the root endpoint ("/").
    
    This change facilitates user interaction and provides a visual interface for the application.
    
    No breaking changes are introduced.

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import logging
+from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 from src.api.v1.check_pdf_forms.router import router_pdf_check
 from src.core import settings
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+app = FastAPI(title="Divorce Lawyer Assistant API", version="1.0.0")
 
 app.include_router(router_pdf_check)
 
 app.mount(
     f"/{settings.STATIC_DIR}",
     StaticFiles(directory=settings.STATIC_DIR),
-    name="uploads",
+    name="static",
 )
+
+templates = Jinja2Templates(directory=Path("src/templates"))
+
+
+@app.get("/", response_class=HTMLResponse)
+async def serve_ui(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
