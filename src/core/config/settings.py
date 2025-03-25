45a173a002289d7ac0dc461bf1e7df8acd3eee2c
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-25 17:43:56 +0200

    feat: Add core configuration module
    This commit introduces a new core configuration module, including settings, logging configuration, and initialization.
    
    - `src/core/config/__init__.py`: Initializes the settings.
    - `src/core/config/logging_config.py`: Configures application
      logging using a dictionary configuration.
    - `src/core/config/settings.py`: Defines application settings using environment variables, including model selection and  API keys.  It also sets up storage configurations and provides access to token limits based on the selected AI model.

---
+++ src/core/config/settings.py
@@ -0,0 +1,47 @@
+"""
+Application settings and configuration.
+"""
+
+import os
+from types import MappingProxyType
+
+from dotenv import load_dotenv
+
+from src.core.constants import MODEL_TOKEN_LIMITS, STATIC_DIR, UPLOAD_DIR
+from src.core.exceptions.config import MissingEnvironmentVariable
+from src.core.storage import LocalStorage
+from src.core.storage.interfaces.base_storage_interface import (
+    BaseStorageInterface,
+)
+from src.core.types import TokenLimitsMapping
+
+load_dotenv()
+
+
+class Settings:
+
+    DEBUG: bool = os.getenv("DEBUG", "True") == "True"
+
+    # Required environment variables
+    try:
+        BASE_AI_MODEL: str = os.environ["BASE_AI_MODEL"]
+        OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
+    except KeyError as e:
+        raise MissingEnvironmentVariable(
+            f"Missing required environment variable: {e}"
+        )
+
+    # Directory settings
+    STATIC_DIR: str = STATIC_DIR
+    UPLOAD_DIR: str = UPLOAD_DIR
+    STORAGE: BaseStorageInterface = LocalStorage(path_to_upload_dir=UPLOAD_DIR)
+
+    # Model settings
+    MODEL_TOKEN_LIMITS: TokenLimitsMapping = MappingProxyType(
+        MODEL_TOKEN_LIMITS
+    )
+
+    @property
+    def get_token_limit(self) -> int:
+        """Get token limit for the current model."""
+        return self.MODEL_TOKEN_LIMITS.get(self.BASE_AI_MODEL, 0)
