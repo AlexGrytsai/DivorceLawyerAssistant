b5637819e4e7b05cfddf817e2f94324563642b8d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-26 12:02:23 +0200

    feat: Add exception class for GCP request problems
    Adds `ProblemWithRequestToGCP` exception class, inheriting from `google.api_core.exceptions.ClientError`, to handle exceptions encountered when making requests to Google Cloud Platform.

--- src/core/exceptions/storage.py
+++ src/core/exceptions/storage.py
@@ -1,7 +1,12 @@
+from google.api_core.exceptions import ClientError
 from google.auth.exceptions import GoogleAuthError
 
 
 class ErrorWithAuthenticationInGCP(GoogleAuthError):
+    pass
+
+
+class ProblemWithRequestToGCP(ClientError):
     pass
 
 
