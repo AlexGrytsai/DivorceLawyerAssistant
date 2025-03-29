fab8d0eeba38e4105c6d85915ab2f97e8c9c9a02
Alex Grytsai <a.grytsai1987@gmail.com>
2025-03-29 08:24:18 +0200

    feat: Initialize project settings and static directory
    This commit introduces project settings through the `Settings` class and initializes them. It also creates the "static" directory, ensuring it exists for storing static files. The settings initialization was moved to `src/core/config/__init__.py` and static directory creation simplified in `src/core/__init__.py`.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,11 +1,6 @@
 import os
 
-from .config.settings import Settings
-
-# Initialize settings
-settings = Settings()
-
 # Create the necessary directories
-os.makedirs(settings.STATIC_DIR, exist_ok=True)
+os.makedirs("static", exist_ok=True)
 
-__all__ = ["settings"]
+__all__ = []
