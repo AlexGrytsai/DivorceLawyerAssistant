ae45b583f0fc1a1df8b6f2845354c6cbd4aae8ef
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-04 06:40:06 +0300

    refactor: Refactor schemas for clarity and consistency
    The FileItem and FolderContents schemas were removed as they duplicated information already present in FileSchema and FolderDataSchema. New schemas FileSchemaForFolder and FolderContentsSchema are introduced to provide a more structured representation of folder contents. This improves the overall schema design and maintainability.

--- src/core/storage/shemas.py
+++ src/core/storage/shemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Optional, List, Union, Literal
+from typing import Optional, Literal, List, Union
 
 from pydantic import HttpUrl, BaseModel, Field
 
@@ -17,26 +17,6 @@ class FileDeleteSchema(BaseModel):
     date_deleted: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
-
-
-class FileItem(BaseModel):
-    name: str
-    path: str
-    type: Literal["file"]
-    size: int
-    updated: Optional[str]
-    url: Optional[str] = None
-
-
-class FolderItem(BaseModel):
-    name: str
-    path: str
-    type: Literal["folder"]
-
-
-class FolderContents(BaseModel):
-    current_path: str
-    items: List[Union[FileItem, FolderItem]]
 
 
 class FolderBaseSchema(BaseModel):
@@ -58,3 +38,16 @@ class FolderDeleteSchema(FolderBaseSchema):
     deleted_time: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
+
+
+class FileSchemaForFolder(FileSchema):
+    type: Literal["file"]
+
+
+class FolderItem(FolderDataSchema):
+    type: Literal["folder"]
+
+
+class FolderContentsSchema(BaseModel):
+    current_path: str
+    items: List[Union[FileSchemaForFolder, FolderItem]]
