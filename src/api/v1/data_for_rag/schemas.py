66368177533854a8c84e7b28e931b96b901b38ad
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-07 06:34:11 +0300

    refactor: Rename Index schemas to KnowledgeBase schemas
    This commit refactors the naming of the Index-related schemas to KnowledgeBase schemas. This change provides a more accurate representation of the underlying data structure and improves the overall clarity of the codebase.
    
    - Renamed `IndexRAGSchema` to `KnowledgeBaseSchema`.
    - Renamed `IndexCreateSchema` to `KnowledgeBaseCreateSchema`.
    - Renamed `IndexGetSchema` to `KnowledgeBaseGetSchema`.
    - Updated field and validator names accordingly.

--- src/api/v1/data_for_rag/schemas.py
+++ src/api/v1/data_for_rag/schemas.py
@@ -2,33 +2,35 @@ from fastapi import HTTPException, status
 from pydantic import BaseModel, field_validator
 
 
-class IndexRAGSchema(BaseModel):
-    name_index: str
+class KnowledgeBaseSchema(BaseModel):
+    name_knowledge_base: str
 
-    @field_validator("name_index")
-    def validate_name_index(cls, value: str) -> str:
+    @field_validator("name_knowledge_base")
+    def validate_name_knowledge_base(cls, value: str) -> str:
         if not value:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail={
-                    "error": "'name_index' must not be empty",
-                    "message": "'name_index' must not be empty",
+                    "error": "'name_knowledge_base' must not be empty",
+                    "message": "'name_knowledge_base' must not be empty",
                 },
             )
         if "/" in value:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail={
-                    "error": "'name_index' cannot contain path separators ('/')",
-                    "message": "'name_index' cannot contain path separators ('/')",
+                    "error": "'name_knowledge_base' cannot contain "
+                    "path separators ('/')",
+                    "message": "'name_knowledge_base' cannot contain "
+                    "path separators ('/')",
                 },
             )
         return value
 
 
-class IndexCreateSchema(IndexRAGSchema):
+class KnowledgeBaseCreateSchema(KnowledgeBaseSchema):
     create_time: str
 
 
-class IndexGetSchema(IndexCreateSchema):
+class KnowledgeBaseGetSchema(KnowledgeBaseCreateSchema):
     update_time: str
