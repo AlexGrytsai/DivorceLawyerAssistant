9da6266ca5cf6f882087454bf6041f214dade16d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-08 06:51:57 +0300

    feat: Add Pinecone vector store factory
    This commit introduces a `PineconeVectorStoreFactory` class which implements the `VectorStoreFactory` interface. This allows the application to create `PineconeVectorStore` instances in a standardized way, decoupling the application from the direct instantiation of Pinecone-specific vector stores.
    
    The `__init__.py` file is updated to export the new factory class.

---
+++ src/services/rag_service/vector_stores/pinecone_factory.py
@@ -0,0 +1,20 @@
+from langchain_core.embeddings import Embeddings
+from langchain_pinecone import PineconeVectorStore
+
+from src.services.rag_service.interfaces.vector_store_factory import (
+    VectorStoreFactory,
+)
+
+
+class PineconeVectorStoreFactory(VectorStoreFactory):
+    def create_vector_store(
+        self,
+        index_name: str,
+        namespace: str,
+        embeddings: Embeddings,
+    ) -> PineconeVectorStore:
+        return PineconeVectorStore(
+            index_name=index_name,
+            embedding=embeddings,
+            namespace=namespace,
+        )
