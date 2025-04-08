ea1249c2d2c76d0641058deb54cc22eeacf2d1bd
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-08 10:55:01 +0300

    feat: Implement DirectoryProcessor for processing directories
    This commit introduces a `DirectoryProcessor` class that recursively processes files within a specified directory.
    
    - It retrieves folder contents using `rag_storage.get_folder_contents`.
    - It filters the folder contents to identify files.
    - It utilizes the `FileProcessorFactory` to obtain the appropriate processor for each file type.
    - Processes each file individually, collecting the resulting documents.
    - Includes the directory path in metadata
    
    This change adds the DirectoryProcessor to the __init__.py file.

--- src/services/rag_service/file_processors/__init__.py
+++ src/services/rag_service/file_processors/__init__.py
@@ -1,4 +1,5 @@
+from .directory_processor import DirectoryProcessor
 from .null_processor import NullProcessor
 from .pdf_processor import PDFProcessor
 
-__all__ = ["NullProcessor", "PDFProcessor"]
+__all__ = ["NullProcessor", "PDFProcessor", "DirectoryProcessor"]
