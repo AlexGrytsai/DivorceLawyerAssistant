ed9e37c0b85c95d97f1f946a5ad3cae35d52a7ef
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-09 19:59:49 +0300

    refactor: Expose RAGServiceInterface in __init__.py
    This commit exposes the `RAGServiceInterface` in the `__init__.py` file of the `rag_service.interfaces` package. This makes the interface directly accessible when importing the package, improving code discoverability and usability.

--- src/services/rag_service/interfaces/__init__.py
+++ src/services/rag_service/interfaces/__init__.py
@@ -1,6 +1,7 @@
 from .document_processor_interface import DocumentProcessorInterface
 from .file_processor_interface import FileProcessorInterface
 from .rag_manager_interface import RAGManagerInterface
+from .rag_service_interface import RAGServiceInterface
 from .search_service_interface import SearchServiceInterface
 from .vector_db_interface import VectorDBInterface
 
@@ -10,4 +11,5 @@ __all__ = [
     "FileProcessorInterface",
     "DocumentProcessorInterface",
     "RAGManagerInterface",
+    "RAGServiceInterface",
 ]
