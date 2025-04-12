dfecd4de69661fb2e06cbc692ef341d62ec34e28
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-12 07:25:34 +0300

    feat: Add category management to KnowledgeStorageInterface
    This commit introduces category management methods to the KnowledgeStorageInterface. This includes methods for creating, renaming, deleting, and retrieving categories within a knowledge storage. It also adds the KnowledgeDetailSchema to allow retrieving more detailed information for a knowledge storage.

--- src/knowledge_storage/interfaces/knowledge_storage_interface.py
+++ src/knowledge_storage/interfaces/knowledge_storage_interface.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Union
 
 from src.knowledge_storage.schemas import (
     KnowledgeSchema,
     KnowledgeRenameSchema,
     KnowledgeDeleteSchema,
+    CategorySchema,
+    CategoryRenameSchema,
+    CategoryDeleteSchema,
+    KnowledgeDetailSchema,
 )
 
 
@@ -24,9 +28,35 @@ class KnowledgeStorageInterface(ABC):
         pass
 
     @abstractmethod
-    async def get_storage(self, name: str) -> KnowledgeRenameSchema:
+    async def get_storage(
+        self, name: str, is_detail: bool = False
+    ) -> Union[KnowledgeRenameSchema, KnowledgeDetailSchema]:
         pass
 
     @abstractmethod
     async def list_storages(self) -> List[KnowledgeSchema]:
+        pass
+
+    @abstractmethod
+    async def create_category(
+        self, storage_name: str, name: str
+    ) -> CategorySchema:
+        pass
+
+    @abstractmethod
+    async def rename_category(
+        self, storage_name: str, old_name: str, new_name: str
+    ) -> CategoryRenameSchema:
+        pass
+
+    @abstractmethod
+    async def delete_category(
+        self, storage_name: str, name: str
+    ) -> CategoryDeleteSchema:
+        pass
+
+    @abstractmethod
+    async def get_category(
+        self, storage_name: str, name: str
+    ) -> CategorySchema:
         pass
