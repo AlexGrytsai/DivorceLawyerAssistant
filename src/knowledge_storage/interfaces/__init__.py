c5243cd8004126920c7b46e422e38902cd7506fb
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-12 08:14:23 +0300

    feat: Expose interfaces in knowledge_storage package
    This commit exposes the `KnowledgeStorageInterface` and `KnowledgeCategoryInterface` within the `knowledge_storage.interfaces` package. This allows for easier access and use of these interfaces by other modules.

---
+++ src/knowledge_storage/interfaces/__init__.py
@@ -0,0 +1,4 @@
+from .knowledge_category_interface import KnowledgeCategoryInterface
+from .knowledge_storage_interface import KnowledgeStorageInterface
+
+__all__ = ["KnowledgeCategoryInterface", "KnowledgeStorageInterface"]
