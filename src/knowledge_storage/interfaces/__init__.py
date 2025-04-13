a8766b6f9c04cca50cd91aa3e716a7317ec99001
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-13 09:55:01 +0300

    feat: Expose interfaces in knowledge_storage module
    This commit exposes several interfaces within the `knowledge_storage` module by adding them to the `__all__` list in `__init__.py`.
    
    The added interfaces are:
    - BaseEntityInterface
    - TaggableInterface
    - PermissionInterface
    - VersionalInterface
    - SearchableInterface
    
    This change simplifies importing and using these interfaces from other parts of the application.

--- src/knowledge_storage/interfaces/__init__.py
+++ src/knowledge_storage/interfaces/__init__.py
@@ -1,4 +1,17 @@
+from .base_entity_interface import BaseEntityInterface
 from .knowledge_category_interface import KnowledgeCategoryInterface
 from .knowledge_storage_interface import KnowledgeStorageInterface
+from .permission_interface import PermissionInterface
+from .searchable_interface import SearchableInterface
+from .taggable_interface import TaggableInterface
+from .versionable_interface import VersionalInterface
 
-__all__ = ["KnowledgeCategoryInterface", "KnowledgeStorageInterface"]
+__all__ = [
+    "KnowledgeStorageInterface",
+    "KnowledgeCategoryInterface",
+    "BaseEntityInterface",
+    "TaggableInterface",
+    "PermissionInterface",
+    "VersionalInterface",
+    "SearchableInterface",
+]
