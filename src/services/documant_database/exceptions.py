f91bdf4093897404e4a2c2a17db7da4ca9937537
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-14 17:44:04 +0300

    feat: Add DocumentAlreadyExistsError exception
    This commit introduces a new exception class, `DocumentAlreadyExistsError`, which inherits from the base `DocumentDatabaseError`. This exception is raised when attempting to add a document to the database that already exists.

--- src/services/documant_database/exceptions.py
+++ src/services/documant_database/exceptions.py
@@ -6,6 +6,10 @@ class DocumentNotFoundError(DocumentDatabaseError):
     pass
 
 
+class DocumentAlreadyExistsError(DocumentDatabaseError):
+    pass
+
+
 class DatabaseConnectionError(DocumentDatabaseError):
     pass
 
