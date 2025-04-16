e578498664db75fbd271359e1d134262ab37069d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-16 09:14:47 +0300

    refactor: Add docstrings to storage exceptions
    This commit adds docstrings to the exception classes in the `src/domain/storage/exceptions.py` file. This improves code readability and maintainability by providing clear explanations of the purpose of each exception.

--- src/domain/storage/exceptions.py
+++ src/domain/storage/exceptions.py
@@ -3,32 +3,40 @@ from google.auth.exceptions import GoogleAuthError
 
 
 class ErrorWithAuthenticationInGCP(GoogleAuthError):
+    """Exception raised when authentication with Google Cloud Platform fails."""
     pass
 
 
 class ProblemWithRequestToGCP(ClientError):
+    """Exception raised when a request to Google Cloud Platform encounters an error."""
     pass
 
 
 class ProblemWithBucket(Exception):
+    """Exception raised when there is an issue with a storage bucket."""
     pass
 
 
 class StorageError(Exception):
+    """Base exception for all storage-related errors."""
     pass
 
 
 class NoFileProvided(StorageError):
+    """Exception raised when no file is provided for an operation that requires one."""
     pass
 
 
 class ErrorSavingFile(StorageError):
+    """Exception raised when there is an error saving a file to storage."""
     pass
 
 
 class ErrorUploadingFile(StorageError):
+    """Exception raised when there is an error uploading a file to storage."""
     pass
 
 
 class ErrorDeletingFile(StorageError):
+    """Exception raised when there is an error deleting a file from storage."""
     pass
