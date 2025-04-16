fb01ee3d30e6b6644a2ae8836cbc1e8c1b201a97
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-16 09:04:50 +0300

    refactor: Refactor storage entities for DDD
    This commit refactors the storage entities to better align with Domain-Driven Design (DDD) principles.
    
    - Renamed `FileSchemaForFolder` to `FileForFolder` for clarity.
    - Renamed `FolderRenameSchema` to `FolderRename` and `FolderDeleteSchema` to `FolderDelete` to improve naming consistency.
    - Updated `FolderContentsSchema` to use the new `FileForFolder` entity.

--- src/domain/storage/entities/folder.py
+++ src/domain/storage/entities/folder.py
@@ -3,7 +3,7 @@ from typing import Literal, Union, List, Optional
 
 from pydantic import BaseModel, Field
 
-from src.domain.storage.entities import File
+from src.domain.storage.entities import FileForFolder
 
 
 class Folder(BaseModel):
@@ -16,19 +16,15 @@ class FolderData(Folder):
     update_time: Optional[datetime] = None
 
 
-class FolderRenameSchema(Folder):
+class FolderRename(Folder):
     old_name: str
     folder_path: str
 
 
-class FolderDeleteSchema(Folder):
+class FolderDelete(Folder):
     deleted_time: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
-
-
-class FileSchemaForFolder(File):
-    type: Literal["file"]
 
 
 class FolderItem(FolderData):
@@ -37,4 +33,4 @@ class FolderItem(FolderData):
 
 class FolderContentsSchema(BaseModel):
     current_path: str
-    items: List[Union[FileSchemaForFolder, FolderItem]]
+    items: List[Union[FileForFolder, FolderItem]]
