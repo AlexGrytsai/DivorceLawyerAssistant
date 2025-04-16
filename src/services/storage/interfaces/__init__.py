e25f4a69fdb24ab148148aecca254d5d476da423
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-16 10:12:04 +0300

    refactor: Remove unused __init__.py in storage interfaces
    This commit removes the `__init__.py` file from the `src/services/storage/interfaces` directory. The file was no longer necessary as the interfaces are now directly imported where needed, simplifying the module structure. This change supports the refactoring towards a DDD structure.

--- src/services/storage/interfaces/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.domain.storage.repositories.cloud_storage_interface import (
-    CloudStorageInterface,
-)
-from .base_storage_interface import BaseStorageInterface
-
-__all__ = [
-    "BaseStorageInterface",
-    "CloudStorageInterface",
-]
