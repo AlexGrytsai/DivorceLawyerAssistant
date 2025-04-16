56fda503a85359d1af76f4f78757ca7b268e98a4
Alex Grytsai <a.grytsai1987@gmail.com>
2025-04-16 12:00:30 +0300

    refactor: Remove storage package in favor of DDD storage
    The `src/services/storage` package is removed. This is part of a larger refactoring to move storage concerns into the infrastructure layer, following Domain-Driven Design principles. This change removes a level of abstraction and consolidates storage logic where it belongs.

--- src/services/storage/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.infrastructure.storage.cloud.cloud_storage import CloudStorage
-from src.infrastructure.storage.local.local_storage import LocalStorage
-
-__all__ = ["LocalStorage", "CloudStorage"]
